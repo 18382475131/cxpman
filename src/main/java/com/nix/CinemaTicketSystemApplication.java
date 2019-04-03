6014e77b3018baa1c67e025242cbfbf88ac3abb5
kiss-yu <kiss4400>
2019-04-03 13:39:03 +0800

    sql server

--- src/main/java/com/nix/CinemaTicketSystemApplication.java
+++ src/main/java/com/nix/CinemaTicketSystemApplication.java
@@ -3,7 +3,6 @@ package com.nix;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author 11723
  */
